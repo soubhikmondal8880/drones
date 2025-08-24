aab0f2ef397845a33155545e7cfd7f928775788b
soubhikmondal8880 <soubhikmondal8880@gmail.com>
2025-08-24 09:10:39 +0530

    Updated login page and restored README.md

--- js/login.js
+++ js/login.js
@@ -13,47 +13,5 @@ $(document).ready(function () {
     }
   });
 
-  // Handle Form Submission
-  $("#loginForm").on("submit", function (e) {
-    e.preventDefault();
 
-    let email = $("#email").val().trim();
-    let password = $("#password").val().trim();
-    let isValid = true;
-
-    // Reset messages
-    $(".error-message").text("");
-    $("#successMessage").hide();
-
-    // Email validation
-    if (email === "") {
-      $("#emailError").text("Email is required");
-      isValid = false;
-    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-      $("#emailError").text("Enter a valid email address");
-      isValid = false;
-    }
-
-    // Password validation
-    if (password === "") {
-      $("#passwordError").text("Password is required");
-      isValid = false;
-    } else if (password.length < 6) {
-      $("#passwordError").text("Password must be at least 6 characters");
-      isValid = false;
-    }
-
-    // If valid -> Show Success
-    if (isValid) {
-      $("#successMessage")
-        .text(" Login successful! Redirecting...")
-        .css("color", "#00ff99")
-        .fadeIn();
-
-      // Redirect after 1.4 seconds
-      setTimeout(() => {
-        window.location.href = "../checkout.html"; //login page
-      }, 1400);
-    }
-  });
 });
